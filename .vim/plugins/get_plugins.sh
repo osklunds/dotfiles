6c1f11fcf5bc55138b7e980710933f911cf76d8f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-02-10 20:42:56 +0100

    Add plugins as submodules

--- .vim/plugins/get_plugins.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git clone https://github.com/dense-analysis/ale.git
-git clone https://github.com/jlanzarotta/bufexplorer.git
-git clone https://github.com/itchyny/lightline.vim
-git clone https://github.com/preservim/nerdtree.git
-git clone https://github.com/tpope/vim-commentary.git
-git clone https://github.com/tpope/vim-pathogen.git
-
