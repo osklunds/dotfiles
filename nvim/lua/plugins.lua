4e5de1f3363e7b4418512fb1b2daafd5cf17422d
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-03-26 10:22:24 +0200

    Add nvim config that enables lsp for rust

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,12 @@
+
+local lspconfig = require('lspconfig')
+lspconfig.pyright.setup {}
+lspconfig.tsserver.setup {}
+lspconfig.rust_analyzer.setup {
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+}
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
