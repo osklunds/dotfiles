36b2fd6a0679be4c09d1aff06a72f73523593c4f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-03-31 20:00:00 +0200

    Organize git aliases

--- .aliases.sh
+++ .aliases.sh
@@ -5,24 +5,37 @@ alias ft='grep -nr . -ie '
 alias ff='find . | grep -i '
 
 # Git
-alias co='git add -A && coa'
-alias coa='git commit -m'
-alias gs='git status'
-alias gl='git log'
-alias gc='git restore --staged $(git rev-parse --show-toplevel) && git checkout $(git rev-parse --show-toplevel) && git clean -f && gs'
-alias gco='git checkout'
-alias gd='git diff'
-alias gpl='git pull'
-alias gps='git push --all origin'
-alias guc='git reset "HEAD^"'
+
+## Misc
 alias ga='git add'
-alias gm='git merge'
-alias gb='git branch'
 alias gr='cd $(git rev-parse --show-toplevel)'
 
+## Commit
+alias co='ga -A && coa'
+alias coa='git commit -m'
+
+## Info
+alias gs='git status'
+alias gl='git log'
+alias gd='git diff'
+alias gdt='git difftool -t vimdiff'
+
+## Push/pull
+alias gpl='git pull'
+alias gps='git push --all origin'
+
+## Clean up
+alias guc='git reset "HEAD^"'
+alias gc='git restore --staged $(git rev-parse --show-toplevel) && git checkout $(git rev-parse --show-toplevel) && git clean -f && gs'
+alias gco='git checkout'
 grt() {
     git revert --no-commit "$1"..HEAD && co "Reverted to $1"
 }
+
+## Branches
+alias gm='git merge'
+alias gmt='git mergetool -t sk'
+alias gb='git branch'
 
 # Editing dotfiles
 alias ev='e ~/.vim/vimrc'
