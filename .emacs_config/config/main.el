d25088e82a9203a8d4c5b1130777dd4f0b9b08b9
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-08-09 21:08:14 +0200

    Remove unnecessary layout fun set

--- .emacs_config/config/main.el
+++ .emacs_config/config/main.el
@@ -658,8 +658,6 @@
    buffer-b)
   (other-window 1))
 
-(setc vdiff-2way-layout-function 'vdiff-2way-layout-function-default)
-
 (advice-add 'vdiff-buffers :after (lambda (&rest r)
                                     (other-window 1)))
                                              
