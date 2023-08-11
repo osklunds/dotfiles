7448adf836ab0d5652be40515363eaeb026cc3be
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-08-11 07:31:09 +0200

    Clean up colors

--- .emacs_config/config/colors.el
+++ .emacs_config/config/colors.el
@@ -24,7 +24,6 @@
   (ol-set-face-attribute 'default :height 90))
 
 (ol-set-face-attribute 'default :foreground "#000000" :background "#ffffff")
-
 (ol-set-face-attribute 'font-lock-comment-face :foreground "#50a14f")
 (ol-set-face-attribute 'font-lock-string-face :foreground "#d78700")
 
@@ -34,7 +33,7 @@
                     (color-darken-name
                      (face-attribute 'default :background) 3))
 
-;; TODO> Do this:
+;; TODO Do this:
 ;; https://emacs.stackexchange.com/questions/28825/how-do-you-set-colors-for-term
 (ol-inherit-face-attribute 'term-color-black 'default)
 
@@ -58,47 +57,47 @@
 ;; vdiff
 ;; ---------------------------------------------------------------------------
 
+(ol-inherit-face-attribute 'vdiff-closed-fold-face 'magit-diff-hunk-heading-highlight)
+
 (defun ol-vdiff-set-face (face-to-set face-to-copy-from)
   (let ((face-val (face-attribute face-to-copy-from :background)))
     (ol-set-face-attribute face-to-set
-                        :inherit nil
-                        :extend t
-                        :background face-val
-                        :foreground nil)))
+                           :inherit nil
+                           :extend t
+                           :background face-val
+                           :foreground nil)))
 
 (ol-vdiff-set-face 'vdiff-addition-face 'magit-diff-added-highlight)
 (ol-vdiff-set-face 'vdiff-refine-added 'magit-diff-added-highlight)
 (ol-vdiff-set-face 'vdiff-change-face 'magit-diff-base)
 (ol-vdiff-set-face 'vdiff-refine-changed 'magit-diff-base-highlight)
-(ol-vdiff-set-face 'vdiff-subtraction-face 'magit-diff-removed)
-
-(ol-inherit-face-attribute 'vdiff-closed-fold-face 'magit-diff-hunk-heading-highlight)
+(ol-vdiff-set-face 'vdiff-subtraction-face 'magit-diff-removed-highlight)
 
 ;; ---------------------------------------------------------------------------
 ;; ediff
 ;; ---------------------------------------------------------------------------
 
-;; These actually made some more sense once I understood them. In ediff, there's a "current"
-;; diff, and "other" diffs. The currently selected diff is highlighted using these
-;; "current" faces below. The non-selected other diffs are highlighted alternatingly
-;;with the odd and even faces.
+;; These actually made some more sense once I understood them. In ediff, there's
+;; a "current" diff, and "other" diffs. The currently selected diff is
+;; highlighted using these "current" faces below. The non-selected other diffs
+;; are highlighted alternatingly with the odd and even faces.
 
 (ol-inherit-face-attribute 'ediff-current-diff-A        'magit-diff-removed)
 (ol-inherit-face-attribute 'ediff-current-diff-B        'magit-diff-added)
 (ol-inherit-face-attribute 'ediff-current-diff-C        'magit-diff-added)
 (ol-inherit-face-attribute 'ediff-current-diff-Ancestor 'magit-diff-base)
 
-(ol-inherit-face-attribute 'ediff-fine-diff-A        'magit-diff-removed-highlight)
-(ol-inherit-face-attribute 'ediff-fine-diff-B        'magit-diff-added-highlight)
-(ol-inherit-face-attribute 'ediff-fine-diff-C        'magit-diff-added-highlight)
-(ol-inherit-face-attribute 'ediff-fine-diff-Ancestor 'magit-diff-base-highlight)
+(ol-inherit-face-attribute 'ediff-fine-diff-A           'magit-diff-removed-highlight)
+(ol-inherit-face-attribute 'ediff-fine-diff-B           'magit-diff-added-highlight)
+(ol-inherit-face-attribute 'ediff-fine-diff-C           'magit-diff-added-highlight)
+(ol-inherit-face-attribute 'ediff-fine-diff-Ancestor    'magit-diff-base-highlight)
 
-(ol-inherit-face-attribute 'ediff-even-diff-A        'magit-diff-removed)
-(ol-inherit-face-attribute 'ediff-even-diff-B        'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-even-diff-C        'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-even-diff-Ancestor 'magit-diff-base)
+(ol-inherit-face-attribute 'ediff-even-diff-A           'magit-diff-removed)
+(ol-inherit-face-attribute 'ediff-even-diff-B           'magit-diff-added)
+(ol-inherit-face-attribute 'ediff-even-diff-C           'magit-diff-added)
+(ol-inherit-face-attribute 'ediff-even-diff-Ancestor    'magit-diff-base)
 
-(ol-inherit-face-attribute 'ediff-odd-diff-A        'magit-diff-removed)
-(ol-inherit-face-attribute 'ediff-odd-diff-B        'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-odd-diff-C        'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-odd-diff-Ancestor 'magit-diff-base)
+(ol-inherit-face-attribute 'ediff-odd-diff-A            'magit-diff-removed)
+(ol-inherit-face-attribute 'ediff-odd-diff-B            'magit-diff-added)
+(ol-inherit-face-attribute 'ediff-odd-diff-C            'magit-diff-added)
+(ol-inherit-face-attribute 'ediff-odd-diff-Ancestor     'magit-diff-base)
