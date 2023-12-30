9aa10a6cdb655a9eb5e3b33b1c36202898cdb171
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2023-12-30 09:22:28 +0100

    Clean up colors

--- .emacs_config/config/colors.el
+++ .emacs_config/config/colors.el
@@ -5,118 +5,137 @@
 ;; Helpers
 ;; -----------------------------------------------------------------------------
 
-;; TODO: Maybe use custom-theme-set-faces instead
-(defun ol-set-face-attribute (face &rest properties)
+(defun ol-set-face (face &rest properties)
   (apply 'set-face-attribute (append (list face nil) properties)))
 
-;; TODO: unset all properties (foreground etc...) the proper way
-(defun ol-inherit-face-attribute (face-to-update face-to-inherit-from)
-  (ol-set-face-attribute face-to-update
-                         :inherit face-to-inherit-from
-                         :foreground 'unspecified
-                         :background 'unspecified))
+(defun ol-copy-face (to attribute from)
+  (ol-set-face to attribute (face-attribute from attribute)))
+
+(defun ol-copy-face-fg-bg (to from)
+  (ol-copy-face to :foreground from)
+  (ol-copy-face to :background from))
 
 ;; -----------------------------------------------------------------------------
 ;; General
 ;; -----------------------------------------------------------------------------
 
-;; TODO: create variables for colors, that are retrieved from the relevant face
+(ol-set-face 'default :height 90)
 
-(ol-set-face-attribute 'default :height 90)
+(ol-set-face 'default :foreground "black" :background "white")
+(ol-set-face 'font-lock-comment-face :foreground "#5f8700")
+(ol-set-face 'font-lock-string-face :foreground "#d78700")
 
-(ol-set-face-attribute 'default :foreground "#000000" :background "#ffffff")
-(ol-set-face-attribute 'font-lock-comment-face :foreground "#5f8700")
-(ol-set-face-attribute 'font-lock-string-face :foreground "#d78700")
+;; -----------------------------------------------------------------------------
+;; Org mode
+;;------------------------------------------------------------------------------
 
-(ol-set-face-attribute 'org-block :background
-                    (color-darken-name
-                     (face-attribute 'default :background) 3))
+(ol-set-face 'org-block :background
+             (color-darken-name
+              (face-attribute 'default :background) 3))
+
+;; -----------------------------------------------------------------------------
+;; Terminal
+;;------------------------------------------------------------------------------
 
 ;; Hack to do it like this. If done directly, colors aren't set it seems
 (defun ol-set-term-colors ()
-  ;; TODO Do this:
+  ;; TODO Do this, setting all colors:
   ;; https://emacs.stackexchange.com/questions/28825/how-do-you-set-colors-for-term
-  (ol-inherit-face-attribute 'term-color-black 'default)
-  (ol-inherit-face-attribute 'term 'default))
+  (ol-set-face 'term-color-black :foreground "black" :background "white")
+  (ol-set-face 'term :foreground "black" :background "white"))
 
 (add-hook 'term-mode-hook 'ol-set-term-colors)
 
-(ol-set-face-attribute 'mode-line
-                    :background "#D7E4E8"
-                    :overline 'unspecified
-                    :underline 'unspecified)
+;; -----------------------------------------------------------------------------
+;; Modeline
+;;------------------------------------------------------------------------------
 
-(ol-set-face-attribute 'mode-line-inactive
-                    :background "#E9EDED"
-                    :overline 'unspecified
-                    :underline 'unspecified)
+(ol-set-face 'mode-line
+             :background "#d7e4e8"
+             :overline 'unspecified
+             :underline 'unspecified)
 
-(ol-set-face-attribute 'ol-buffer-name-mode-line-face
-                       :weight 'bold)
+(ol-set-face 'mode-line-inactive
+             :background "#e9eded"
+             :overline 'unspecified
+             :underline 'unspecified)
 
-(ol-inherit-face-attribute 'ol-evil-normal-state-mode-line-face 'font-lock-comment-face)
-(ol-inherit-face-attribute 'ol-evil-insert-state-mode-line-face 'font-lock-keyword-face)
-(ol-inherit-face-attribute 'ol-evil-visual-state-mode-line-face 'warning)
-(ol-inherit-face-attribute 'ol-evil-emacs-state-mode-line-face 'font-lock-builtin-face)
+(ol-set-face 'ol-buffer-name-mode-line-face
+             :weight 'bold)
+
+(ol-copy-face-fg-bg 'ol-evil-normal-state-mode-line-face 'font-lock-comment-face)
+(ol-copy-face-fg-bg 'ol-evil-insert-state-mode-line-face 'font-lock-keyword-face)
+(ol-copy-face-fg-bg 'ol-evil-visual-state-mode-line-face 'warning)
+(ol-copy-face-fg-bg 'ol-evil-emacs-state-mode-line-face 'font-lock-builtin-face)
 
 (dolist (face '(ol-evil-normal-state-mode-line-face
                 ol-evil-insert-state-mode-line-face
                 ol-evil-visual-state-mode-line-face
                 ol-evil-emacs-state-mode-line-face
                 ol-evil-operator-state-mode-line-face))
-  (ol-set-face-attribute face :weight 'bold))
+  (ol-set-face face :weight 'bold))
+
+;; -----------------------------------------------------------------------------
+;; Ivy and company consistency
+;;------------------------------------------------------------------------------
 
 (dolist (face '(ivy-minibuffer-match-face-1
                 ivy-minibuffer-match-face-2
                 ivy-minibuffer-match-face-3
                 ivy-minibuffer-match-face-4))
-  (ol-set-face-attribute face :weight 'bold)
-  (ol-set-face-attribute face :foreground "#4078f2")
-  (ol-set-face-attribute face :background "#ffffff"))
+  (ol-copy-face-fg-bg face 'company-tooltip-common)
+  (ol-set-face face :weight 'bold)
+  (ol-set-face face :background "white"))
 
-(ol-set-face-attribute 'ivy-current-match :weight 'bold)
-(ol-set-face-attribute 'ivy-current-match :background "#D7E4E8")
+(ol-set-face 'ivy-current-match :weight 'bold)
+(ol-set-face 'ivy-current-match :background "#D7E4E8")
 
-(ol-set-face-attribute 'company-box-background :background "#ffffff")
-(ol-set-face-attribute 'company-box-selection :background "#D7E4E8")
+(ol-set-face 'company-box-background :background "white")
+(ol-set-face 'company-box-selection :background "#D7E4E8")
 
 ;; ---------------------------------------------------------------------------
 ;; Magit
 ;; ---------------------------------------------------------------------------
 
-(ol-set-face-attribute 'magit-blame-margin :background "#e4e4e4")
-(ol-set-face-attribute 'magit-log-date :foreground "#da8548")
+(ol-set-face 'magit-blame-margin :background "#e4e4e4")
+(ol-set-face 'magit-log-date :foreground "#da8548")
 
 ;; ---------------------------------------------------------------------------
 ;; vdiff
 ;; ---------------------------------------------------------------------------
 
-(ol-inherit-face-attribute 'vdiff-closed-fold-face 'magit-diff-hunk-heading-highlight)
+(ol-copy-face-fg-bg 'vdiff-closed-fold-face 'magit-diff-hunk-heading-highlight)
 
 (defun ol-vdiff-set-face (face-to-set face-val)
-  (ol-set-face-attribute face-to-set
-                         :inherit nil
-                         :extend t
-                         :background face-val
-                         :foreground 'unspecified))
+  (ol-set-face face-to-set
+               :inherit nil
+               :extend t
+               :background face-val
+               :foreground 'unspecified))
+
+(defconst ol-diff-green "#9fec9d")
+(defconst ol-diff-red "#e45649")
+(defconst ol-diff-dark-orange "#ffd787")
+(defconst ol-diff-light-orange "#f6eee8")
+
 ;; Add
-(ol-vdiff-set-face 'vdiff-addition-face "#9FEC9D")
-(ol-vdiff-set-face 'vdiff-refine-added "#9FEC9D")
+(ol-vdiff-set-face 'vdiff-addition-face ol-diff-green)
+(ol-vdiff-set-face 'vdiff-refine-added ol-diff-green)
 
 ;; Delete
-(ol-vdiff-set-face 'vdiff-subtraction-face "#E45649")
+(ol-vdiff-set-face 'vdiff-subtraction-face ol-diff-red)
 
 ;; Change
-(ol-vdiff-set-face 'vdiff-refine-changed "#ffd787")
-(ol-vdiff-set-face 'vdiff-change-face "#F6EEE8")
+(ol-vdiff-set-face 'vdiff-refine-changed ol-diff-dark-orange)
+(ol-vdiff-set-face 'vdiff-change-face ol-diff-light-orange)
 
 ;; ---------------------------------------------------------------------------
 ;; smerge
 ;; ---------------------------------------------------------------------------
 
-(ol-set-face-attribute 'smerge-base :background "#ffd787")
-(ol-set-face-attribute 'smerge-lower :background "#E45649")
-(ol-set-face-attribute 'smerge-upper :background "#9FEC9D")
+(ol-set-face 'smerge-base :background ol-diff-dark-orange)
+(ol-set-face 'smerge-lower :background ol-diff-red)
+(ol-set-face 'smerge-upper :background ol-diff-green)
 
 ;; ---------------------------------------------------------------------------
 ;; ediff
@@ -127,22 +146,22 @@
 ;; highlighted using these "current" faces below. The non-selected other diffs
 ;; are highlighted alternatingly with the odd and even faces.
 
-(ol-inherit-face-attribute 'ediff-current-diff-A        'magit-diff-removed)
-(ol-inherit-face-attribute 'ediff-current-diff-B        'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-current-diff-C        'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-current-diff-Ancestor 'magit-diff-base)
+(ol-copy-face-fg-bg 'ediff-current-diff-A        'magit-diff-removed)
+(ol-copy-face-fg-bg 'ediff-current-diff-B        'magit-diff-added)
+(ol-copy-face-fg-bg 'ediff-current-diff-C        'magit-diff-added)
+(ol-copy-face-fg-bg 'ediff-current-diff-Ancestor 'magit-diff-base)
 
-(ol-inherit-face-attribute 'ediff-fine-diff-A           'magit-diff-removed-highlight)
-(ol-inherit-face-attribute 'ediff-fine-diff-B           'magit-diff-added-highlight)
-(ol-inherit-face-attribute 'ediff-fine-diff-C           'magit-diff-added-highlight)
-(ol-inherit-face-attribute 'ediff-fine-diff-Ancestor    'magit-diff-base-highlight)
+(ol-copy-face-fg-bg 'ediff-fine-diff-A           'magit-diff-removed-highlight)
+(ol-copy-face-fg-bg 'ediff-fine-diff-B           'magit-diff-added-highlight)
+(ol-copy-face-fg-bg 'ediff-fine-diff-C           'magit-diff-added-highlight)
+(ol-copy-face-fg-bg 'ediff-fine-diff-Ancestor    'magit-diff-base-highlight)
 
-(ol-inherit-face-attribute 'ediff-even-diff-A           'magit-diff-removed)
-(ol-inherit-face-attribute 'ediff-even-diff-B           'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-even-diff-C           'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-even-diff-Ancestor    'magit-diff-base)
+(ol-copy-face-fg-bg 'ediff-even-diff-A           'magit-diff-removed)
+(ol-copy-face-fg-bg 'ediff-even-diff-B           'magit-diff-added)
+(ol-copy-face-fg-bg 'ediff-even-diff-C           'magit-diff-added)
+(ol-copy-face-fg-bg 'ediff-even-diff-Ancestor    'magit-diff-base)
 
-(ol-inherit-face-attribute 'ediff-odd-diff-A            'magit-diff-removed)
-(ol-inherit-face-attribute 'ediff-odd-diff-B            'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-odd-diff-C            'magit-diff-added)
-(ol-inherit-face-attribute 'ediff-odd-diff-Ancestor     'magit-diff-base)
+(ol-copy-face-fg-bg 'ediff-odd-diff-A            'magit-diff-removed)
+(ol-copy-face-fg-bg 'ediff-odd-diff-B            'magit-diff-added)
+(ol-copy-face-fg-bg 'ediff-odd-diff-C            'magit-diff-added)
+(ol-copy-face-fg-bg 'ediff-odd-diff-Ancestor     'magit-diff-base)
