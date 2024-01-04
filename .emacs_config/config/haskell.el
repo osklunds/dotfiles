5b050cc36e182d4a9746ffffc32e278e7c500575
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-01-04 17:33:29 +0100

    Adjust haskell indentation

--- .emacs_config/config/haskell.el
+++ .emacs_config/config/haskell.el
@@ -3,3 +3,9 @@
 (require 'haskell-mode-autoloads)
 
 (add-hook 'haskell-mode-hook 'lsp)
+
+(setc haskell-indentation-layout-offset 4)
+(setc haskell-indentation-starter-offset 4)
+(setc haskell-indentation-left-offset 4)
+(setc haskell-indentation-where-pre-offset 4)
+(setc haskell-indentation-where-post-offset 4)
