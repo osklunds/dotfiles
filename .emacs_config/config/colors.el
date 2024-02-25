af8eabcba0b2cf9a3d7802721f4e411b1b0f4306
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-02-25 07:13:29 +0100

    Make colors better for terminal emacs

--- .emacs_config/config/colors.el
+++ .emacs_config/config/colors.el
@@ -21,9 +21,22 @@
 
 (ol-set-face 'default :height 90)
 
-(ol-set-face 'default :foreground "black" :background "white")
+(defconst ol-white "#ffffff") ;; ff works better than white in terminal
+(defconst ol-black "#000000")
+
+(ol-set-face 'default :foreground ol-black :background ol-white)
 (ol-set-face 'font-lock-comment-face :foreground "#5f8700")
 (ol-set-face 'font-lock-string-face :foreground "#d78700")
+
+(unless (display-graphic-p)
+  (ol-set-face 'hl-line :background "#eeeeee"))
+
+;; -----------------------------------------------------------------------------
+;; Evil
+;;------------------------------------------------------------------------------
+
+(unless (display-graphic-p)
+  (ol-set-face 'lazy-highlight :background "#c2d3f7" :foreground ol-white))
 
 ;; -----------------------------------------------------------------------------
 ;; Org mode
@@ -41,8 +54,8 @@
 (defun ol-set-term-colors ()
   ;; TODO Do this, setting all colors:
   ;; https://emacs.stackexchange.com/questions/28825/how-do-you-set-colors-for-term
-  (ol-set-face 'term-color-black :foreground "black" :background "white")
-  (ol-set-face 'term :foreground "black" :background "white"))
+  (ol-set-face 'term-color-black :foreground ol-black :background ol-white)
+  (ol-set-face 'term :foreground ol-black :background ol-white))
 
 (add-hook 'term-mode-hook 'ol-set-term-colors)
 
@@ -51,9 +64,12 @@
 ;;------------------------------------------------------------------------------
 
 (ol-set-face 'mode-line
-             :background "#d7e4e8"
              :overline 'unspecified
              :underline 'unspecified)
+
+(if (display-graphic-p)
+    (ol-set-face 'mode-line :background "#d7e4e8")
+  (ol-set-face 'mode-line :background "#cccccc"))
 
 (ol-set-face 'mode-line-inactive
              :background "#e9eded"
@@ -85,13 +101,16 @@
                 ivy-minibuffer-match-face-4))
   (ol-copy-face-fg-bg face 'company-tooltip-common)
   (ol-set-face face :weight 'bold)
-  (ol-set-face face :background "white"))
+  (ol-set-face face :background ol-white))
+
+(defconst ol-completion-selection-color "#d7e4e8")
 
 (ol-set-face 'ivy-current-match :weight 'bold)
-(ol-set-face 'ivy-current-match :background "#D7E4E8")
+(ol-set-face 'ivy-current-match :background ol-completion-selection-color)
 
-(ol-set-face 'company-box-background :background "white")
-(ol-set-face 'company-box-selection :background "#D7E4E8")
+(ol-set-face 'company-box-background :background ol-white)
+(ol-set-face 'company-box-selection :background ol-completion-selection-color)
+
 
 ;; ---------------------------------------------------------------------------
 ;; Magit
@@ -99,6 +118,25 @@
 
 (ol-set-face 'magit-blame-margin :background "#e4e4e4")
 (ol-set-face 'magit-log-date :foreground "#da8548")
+
+(defconst ol-diff-green "#9fec9d")
+(defconst ol-diff-dark-red "#e45649")
+(defconst ol-diff-light-red "#f5d9d6")
+(defconst ol-diff-dark-orange "#ffd787")
+(defconst ol-diff-light-orange "#f6eee8")
+
+(defun ol-magit-diff-set-face (face-to-set face-val)
+  (ol-set-face face-to-set
+               :background face-val
+               :foreground 'unspecified))
+
+(unless (display-graphic-p)
+  (ol-magit-diff-set-face 'magit-diff-added-highlight   ol-diff-green)
+  (ol-magit-diff-set-face 'magit-diff-added             ol-diff-green)
+  (ol-magit-diff-set-face 'magit-diff-base-highlight    ol-diff-dark-orange)
+  (ol-magit-diff-set-face 'magit-diff-base              ol-diff-dark-orange)
+  (ol-magit-diff-set-face 'magit-diff-removed-highlight ol-diff-light-red)
+  (ol-magit-diff-set-face 'magit-diff-removed           ol-diff-light-red))
 
 ;; ---------------------------------------------------------------------------
 ;; vdiff
@@ -113,17 +151,12 @@
                :background face-val
                :foreground 'unspecified))
 
-(defconst ol-diff-green "#9fec9d")
-(defconst ol-diff-red "#e45649")
-(defconst ol-diff-dark-orange "#ffd787")
-(defconst ol-diff-light-orange "#f6eee8")
-
 ;; Add
 (ol-vdiff-set-face 'vdiff-addition-face ol-diff-green)
 (ol-vdiff-set-face 'vdiff-refine-added ol-diff-green)
 
 ;; Delete
-(ol-vdiff-set-face 'vdiff-subtraction-face ol-diff-red)
+(ol-vdiff-set-face 'vdiff-subtraction-face ol-diff-dark-red)
 
 ;; Change
 (ol-vdiff-set-face 'vdiff-refine-changed ol-diff-dark-orange)
@@ -133,9 +166,12 @@
 ;; smerge
 ;; ---------------------------------------------------------------------------
 
-(ol-set-face 'smerge-base :background ol-diff-dark-orange)
-(ol-set-face 'smerge-lower :background ol-diff-red)
-(ol-set-face 'smerge-upper :background ol-diff-green)
+(defun ol-smerge-set-face (face-to-set face-val)
+  (ol-set-face face-to-set :background face-val :foreground ol-black :weight 'normal))
+
+(ol-smerge-set-face 'smerge-base ol-diff-dark-orange)
+(ol-smerge-set-face 'smerge-lower ol-diff-light-red)
+(ol-smerge-set-face 'smerge-upper ol-diff-green)
 
 ;; ---------------------------------------------------------------------------
 ;; ediff
