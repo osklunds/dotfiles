8aa0d250e2c1aa09241bc8c469c004ac7c9e0255
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-06-16 14:01:29 +0200

    Add java dbg snippet

--- .emacs_config/config/java-config.el
+++ .emacs_config/config/java-config.el
@@ -3,3 +3,8 @@
 (ol-require-external "javac")
 
 (add-hook 'java-mode-hook #'lsp)
+
+(define-abbrev-table 'java-mode-abbrev-table
+  '(
+    ("dbg" "System.out.printf(\"oskar: %s%n\", @@);")
+    ))
