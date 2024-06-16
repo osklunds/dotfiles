7136597dc1924ee3644b2277246e6823254c7830
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-06-16 14:01:37 +0200

    Run current java test

--- .emacs_config/config/java-config.el
+++ .emacs_config/config/java-config.el
@@ -8,3 +8,15 @@
   '(
     ("dbg" "System.out.printf(\"oskar: %s%n\", @@);")
     ))
+
+(defun ol-java-run-current-test ()
+  (interactive)
+  (save-buffer)
+  (when-let* ((test-name (which-function))
+              (cmd (concat "mvn test -Dtest=\"#" test-name "\"\n")))
+    (dolist (window (window-list))
+      (with-current-buffer (window-buffer window)
+        (when (eq major-mode 'vterm-mode)
+          (vterm-send-string cmd))))))
+
+(ol-evil-define-key 'normal java-mode-map "C-c r" 'ol-java-run-current-test)
