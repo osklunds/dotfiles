4a325970d3a4751ba61ef6cc14405d6fdaf39056
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-27 10:16:20 +0200

    Function for running current rust test

--- .emacs_config/config/rust.el
+++ .emacs_config/config/rust.el
@@ -21,3 +21,26 @@
     ("as" "// Assert")
     ))
 
+(defun ol-rust-name-of-function ()
+  (if-let ((name (ol-rust-get-function-on-current-line)))
+      name
+    (save-excursion
+      (rust-beginning-of-defun)
+      (ol-rust-name-of-function))))
+
+(defun ol-rust-get-function-on-current-line ()
+  (save-excursion
+    (beginning-of-line)
+    (ol-regexp-group "^fn \\([a-zA-Z0-9_]+\\)" (thing-at-point 'line t) 1)))
+
+(defun ol-rust-run-current-test ()
+  (interactive)
+  (save-buffer)
+  (when-let* ((test-name (ol-rust-name-of-function))
+              (cmd (concat "ct " test-name "\n")))
+    (dolist (window (window-list))
+      (with-current-buffer (window-buffer window)
+        (when (eq major-mode 'vterm-mode)
+          (vterm-send-string cmd))))))
+
+(ol-evil-define-key 'normal rust-mode-map "C-c r" 'ol-rust-run-current-test)
