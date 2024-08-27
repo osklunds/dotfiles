c86ca0a61ed787b771bb18d1f24dd8324020410b
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-08-27 20:25:47 +0200

    Add headers to rust file, add cargo check

--- .emacs_config/config/rust.el
+++ .emacs_config/config/rust.el
@@ -1,6 +1,11 @@
 
 (require 'rust-mode)
 (ol-require-external "rust-analyzer")
+(ol-require-external "cargo")
+
+;; -----------------------------------------------------------------------------
+;; LSP
+;;------------------------------------------------------------------------------
 
 (add-hook 'rust-mode-hook 'lsp)
 
@@ -12,6 +17,10 @@
 
 (setc lsp-rust-analyzer-completion-add-call-parenthesis nil)
 
+;; -----------------------------------------------------------------------------
+;; Snippets
+;;------------------------------------------------------------------------------
+
 (define-abbrev-table 'rust-mode-abbrev-table
   '(
     ("dbg" "println!(\"oskar: {:?}\", @@);")
@@ -20,6 +29,10 @@
     ("ac" "// Act")
     ("as" "// Assert")
     ))
+
+;; -----------------------------------------------------------------------------
+;; Terminal integration
+;;------------------------------------------------------------------------------
 
 (defun ol-rust-name-of-function ()
   (if-let ((name (ol-rust-get-function-on-current-line)))
@@ -31,16 +44,27 @@
 (defun ol-rust-get-function-on-current-line ()
   (save-excursion
     (beginning-of-line)
-    (ol-regexp-group "^fn \\([a-zA-Z0-9_]+\\)" (thing-at-point 'line t) 1)))
+    (ol-regexp-group "^ *fn \\([a-zA-Z0-9_]+\\)" (thing-at-point 'line t) 1)))
 
 (defun ol-rust-run-current-test ()
   (interactive)
   (save-buffer)
   (when-let* ((test-name (ol-rust-name-of-function))
-              (cmd (concat "ct " test-name "\n")))
-    (dolist (window (window-list))
-      (with-current-buffer (window-buffer window)
-        (when (eq major-mode 'vterm-mode)
-          (vterm-send-string cmd))))))
+              (cmd (concat "ct " test-name)))
+    (ol-send-cmd-to-visible-vterm-buffers cmd)))
 
 (ol-evil-define-key 'normal rust-mode-map "C-c r" 'ol-rust-run-current-test)
+
+(defun ol-rust-cargo-check ()
+  (interactive)
+  (save-buffer)
+  (ol-send-cmd-to-visible-vterm-buffers "cargo check"))
+
+(defun ol-send-cmd-to-visible-vterm-buffers (cmd)
+  (dolist (window (window-list))
+    (with-current-buffer (window-buffer window)
+      (when (eq major-mode 'vterm-mode)
+        (vterm-send-string (concat cmd "\n"))))))
+
+(ol-evil-define-key 'normal rust-mode-map "C-c C-c" 'ol-rust-cargo-check)
+
