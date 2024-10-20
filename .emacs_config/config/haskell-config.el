174bf0e3af82c70c6b5f70f3b25056999b5a2d83
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2024-10-20 07:00:41 +0200

    Load haskell module of buffer

--- .emacs_config/config/haskell-config.el
+++ .emacs_config/config/haskell-config.el
@@ -2,7 +2,15 @@
 (require 'haskell-mode)
 (require 'haskell-mode-autoloads)
 
+;; -----------------------------------------------------------------------------
+;; LSP
+;;------------------------------------------------------------------------------
+
 (add-hook 'haskell-mode-hook 'lsp)
+
+;; -----------------------------------------------------------------------------
+;; Indentation
+;;------------------------------------------------------------------------------
 
 (setc haskell-indentation-layout-offset 4)
 (setc haskell-indentation-starter-offset 4)
@@ -15,3 +23,14 @@
 (add-hook 'haskell-mode-hook
           (lambda ()
             (setq evil-auto-indent nil)))
+
+;; -----------------------------------------------------------------------------
+;; Terminal integration
+;;------------------------------------------------------------------------------
+
+(defun ol-haskell-load-current-module ()
+  (interactive)
+  (when-let ((module (haskell-guess-module-name)))
+    (ol-send-cmd-to-visible-vterm-buffers (concat ":l " module))))
+
+(ol-evil-define-key 'normal haskell-mode-map "C-c l" 'ol-haskell-load-current-module)
