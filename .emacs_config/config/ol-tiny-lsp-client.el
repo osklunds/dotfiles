011c17f68f3be55f0a8868dd39e13a707754f0c1
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-02-15 09:23:07 +0100

    Change to tiny-lsp-client config

--- .emacs_config/config/ol-tiny-lsp-client.el
+++ .emacs_config/config/ol-tiny-lsp-client.el
@@ -5,12 +5,26 @@
 
 (require 'tiny-lsp-client)
 
-(add-hook 'rust-mode-hook 'tlc-mode)
+(setc tlc-log-io t)
+(setc tlc-log-stderr t)
+(setc tlc-log-rust-debug t)
+(setc tlc-log-emacs-debug t)
+(setc tlc-log-to-stdio t)
+
+(add-hook 'rust-mode-hook 'ol-tlc-hook)
+;; (remove-hook 'rust-mode-hook 'ol-tlc-hook)
+
+(defun ol-tlc-hook ()
+  (setc lsp-log-io t)
+  (setq lsp--show-message nil)
+  (tlc-mode)
+  (ol-prefer-tlc-xref)
+  (advice-add 'xref-backend-definitions
+              :filter-return
+              'ol-tlc-xref-debug))
 
 (defun ol-prefer-tlc-xref ()
   (setq-local xref-backend-functions '(tlc-xref-backend lsp--xref-backend t)))
-
-(add-hook 'rust-mode-hook 'ol-prefer-tlc-xref)
 
 (defun ol-tlc-xref-debug (result)
   (if result
@@ -18,7 +32,4 @@
     (message "tiny-lsp-client FAILED"))
   result)
 
-(advice-add 'xref-backend-definitions
-            :filter-return
-            'ol-tlc-xref-debug)
 
