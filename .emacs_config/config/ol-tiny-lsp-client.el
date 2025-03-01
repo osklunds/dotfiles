058084acad35713e3ad17ad70aa571147bd93498
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-01 16:30:29 +0100

    Clean up strange tlc configs

--- .emacs_config/config/ol-tiny-lsp-client.el
+++ .emacs_config/config/ol-tiny-lsp-client.el
@@ -7,7 +7,7 @@
 
 (setc tlc-log-io t)
 (setc tlc-log-stderr t)
-(setc tlc-log-rust-debug nil)
+(setc tlc-log-rust-debug t)
 (setc tlc-log-emacs-debug nil)
 (setc tlc-log-to-stdio nil)
 
@@ -19,29 +19,23 @@
 (add-hook 'c++-mode-hook 'ol-tlc-hook)
 (add-hook 'erlang-mode-hook 'ol-tlc-hook)
 (add-hook 'rust-mode-hook 'ol-tlc-hook)
-;; (remove-hook 'rust-mode-hook 'ol-tlc-hook)
 
 (defun ol-tlc-hook ()
   (setc lsp-log-io t)
   (setq lsp--show-message nil)
   (tlc-mode)
   (ol-prefer-tlc-xref)
+  (ol-prefer-tlc-capf)
   )
 
 (defun ol-prefer-tlc-xref ()
-  (setq-local xref-backend-functions '(tlc-xref-backend lsp--xref-backend t)))
+  (setq-local xref-backend-functions '(tlc-xref-backend t)))
 
-(defun ol-tlc-xref-debug (result)
-  (if result
-      (message "tiny-lsp-client found definition")
-    (message "tiny-lsp-client FAILED"))
-  result)
+(defun ol-prefer-tlc-capf ()
+  (setq-local completion-at-point-functions '(tlc-completion-at-point t)))
 
 (setc tlc-find-root-function 'tlc-dev-find-root-function)
 
-(advice-add 'tlc-xref-backend
-            :filter-return
-            'ol-tlc-xref-debug)
 (ol-define-normal-leader-key "ll" 'tlc-open-log-file)
 (ol-define-normal-leader-key "li" 'tlc-info)
 (ol-define-normal-leader-key "lr" 'tlc-restart-server)
