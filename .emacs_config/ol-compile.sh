e6ee83270c970f57dd2384429d04d77a39f48c34
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-08 06:49:20 +0100

    Make ol-compile.sh standalone without .el file, and add comment

---
+++ .emacs_config/ol-compile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Script for compiling my config files one-by-one. This is useful to check if
+# they are requiring their dependencies properly on their own and can be
+# compiled. One trick is to run this command from compile mode in emacs.
+
+common_setup="
+(progn
+    (setq load-path (append load-path
+                            '(\"~/.emacs_config/config\")
+                            '(\"~/.emacs_config/packages_own/\")
+                            '(\"~/own_repos/tiny-lsp-client/\")
+                            (file-expand-wildcards \"~/.emacs_config/packages/*\")
+                            (file-expand-wildcards \"~/.emacs_config/packages/*/src/*\")
+                            (file-expand-wildcards \"~/.emacs_config/packages/*/clients\")
+                            (file-expand-wildcards \"~/.emacs_config/packages/*/lisp\"))
+          )
+    
+    (setq byte-compile-error-on-warn t)
+)
+"
+
+emacs --batch --eval "$common_setup"
+
+for file in config/*.el; do
+    echo "$file"
+    emacs --batch \
+          --eval "$common_setup" \
+          --eval "(byte-recompile-file \"$file\" t 0)"
+done
+
