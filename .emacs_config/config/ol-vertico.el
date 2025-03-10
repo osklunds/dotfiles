0d5c5e33981048e1c233d1886e3e6c5a141fe3ff
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-10 19:26:53 +0100

    Add basic vertico config

--- .emacs_config/config/ol-vertico.el
+++ .emacs_config/config/ol-vertico.el
@@ -1,6 +1,15 @@
 
 (require 'vertico)
 
+(vertico-mode)
 
+(ol-override-key "C-j" 'switch-to-buffer)
+
+(ol-define-key minibuffer-local-map "C-j" 'next-line)
+(ol-define-key minibuffer-local-map "C-k" 'previous-line)
+(ol-define-key minibuffer-local-map "C-n" 'minibuffer-keyboard-quit)
+(ol-define-key minibuffer-local-map 'tab 'vertico-exit)
+(ol-define-key minibuffer-local-map 'return 'vertico-exit-input)
+(ol-define-key minibuffer-local-map "M-i" 'vertico-insert)
 
 (provide 'ol-vertico)
