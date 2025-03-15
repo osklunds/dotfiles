681eda02924b4cfa6bad9efb6942819154673444
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-15 19:49:41 +0100

    Add tests and find cmd fun

---
+++ .emacs_config/config/ol-ert.el
@@ -0,0 +1,27 @@
+
+(defun ol-assert-equal (exp act &optional label)
+  (when (not (equal exp act))
+    (message "")
+    (message "")
+    (message "")
+    (message "-----------------------------------------------------------------------------")
+    (message "Assert failed. label: '%s'" label)
+    (message "Exp: '%s'" exp)
+    (message "Act: '%s'" act)
+    (message "-----------------------------------------------------------------------------")
+    (message "")
+    (message "")
+    (message "")
+    (sleep-for 1)
+    )
+  (should (equal exp act)))
+
+(defun ol-assert (exp &optional label)
+  (ol-assert-equal t (not (not exp)) label))
+
+(defun ol-assert-not (exp &optional label)
+  (ol-assert-equal nil exp label))
+
+(ol-evil-define-key 'normal emacs-lisp-mode-map "SPC SPC e" 'ert)
+
+(provide 'ol-ert)
--- .emacs_config/config/ol-new-find.el
+++ .emacs_config/config/ol-new-find.el
@@ -1,5 +1,6 @@
 
 (require 'ol-projectile)
+(require 'ol-ert)
 
 ;; -----------------------------------------------------------------------------
 ;; Root
@@ -16,6 +17,29 @@
 ;; -----------------------------------------------------------------------------
 ;; File name
 ;; -----------------------------------------------------------------------------
+
+;; Even if rg is available locally, it might not be over tramp
+(defconst ol2-find-file-cmds '("rg --files"
+                               "find"
+                               ))
+
+(defun ol2-cmd-available (cmd-with-args)
+  (let ((cmd (car (split-string cmd-with-args " "))))
+    (executable-find cmd 'remote)))
+
+(ert-deftest ol2-cmd-available-test ()
+  (ol-assert     (ol2-cmd-available "rg"))
+  (ol-assert     (ol2-cmd-available "rg --files"))
+  (ol-assert     (ol2-cmd-available "find"))
+  (ol-assert-not (ol2-cmd-available "findd"))
+  )
+
+(defun ol2-find-file-cmd ()
+  (cl-find-if 'ol2-cmd-available ol2-find-file-cmds))
+
+(ert-deftest ol2-find-file-cmd-test ()
+  (ol-assert-equal "rg --files" (ol2-find-file-cmd))
+  )
 
 (defun ol2-dwim-find-file-name (&optional prefer-project-root)
   (interactive "P")
