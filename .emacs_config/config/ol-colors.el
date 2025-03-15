962e05812e035592dcfadf0beaf7669131556dc4
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-15 07:31:44 +0100

    Document set face functions

--- .emacs_config/config/ol-colors.el
+++ .emacs_config/config/ol-colors.el
@@ -4,16 +4,20 @@
 
 (require 'doom-themes)
 
-;; todo copy but only keep this one
 (load-theme 'doom-one-light t)
 
 (defun ol-set-face (face &rest properties)
-  (apply 'set-face-attribute (append (list face nil) properties)))
+  "Wrapper around `set-face-attribute' that sets for all frames."
+  (apply 'set-face-attribute (append `(,face nil) properties)))
 
 (defun ol-copy-face (to attribute from)
+  "Sets `attribute' on `to' based on the value of `attribute' in `from'.
+Maybe inherit would be cleaner to use."
   (ol-set-face to attribute (face-attribute from attribute)))
 
 (defun ol-copy-face-fg-bg (to from)
+  "Sets `:foreground' and `:background' on `to' based on the values in `from'
+Maybe inherit would be cleaner to use."
   (ol-copy-face to :foreground from)
   (ol-copy-face to :background from))
 
