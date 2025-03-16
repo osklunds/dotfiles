ad50cafd1314062688a2d73d097ac0aeb65ad5ed
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-16 16:47:55 +0100

    Project list and discovery

--- .emacs_config/config/ol-project.el
+++ .emacs_config/config/ol-project.el
@@ -1,4 +1,8 @@
 
+;; -----------------------------------------------------------------------------
+;; Project root and name of current buffer
+;; -----------------------------------------------------------------------------
+
 (defvar ol-project-root-functions '(ol-project-git-root))
 
 (defun ol-project-git-root ()
@@ -25,6 +29,37 @@
 (defun ol-project-name-compute ()
   (when-let ((root (ol-project-root)))
     (file-name-nondirectory (directory-file-name root))))
-    
+
+;; -----------------------------------------------------------------------------
+;; List of projects
+;; -----------------------------------------------------------------------------
+
+(defvar ol-project-roots nil)
+
+(defvar ol-projects-search-path '(("~/own_repos" . 1)
+                                  ("~/others_repos" . 1)
+                                  ("~/own_repos/dotfiles/.emacs_config/packages" . 1)
+                                  ("~/Dropbox/Dokument")))
+
+(defun ol-discover-projects ()
+  (interactive)
+  (setq ol-project-roots nil)
+  (dolist (path ol-projects-search-path)
+    (let* ((dir (car path))
+           (depth (or (cdr-safe path) 0)))
+      (ol-discover-projects-in-dir dir depth))))
+
+(defun ol-discover-projects-in-dir (dir depth)
+  (when (file-directory-p dir)
+    (when-let* ((default-directory dir)
+                (root (ol-project-root-compute)))
+      (add-to-list 'ol-project-roots root))
+    (when (> depth 0)
+      (dolist (subdir (directory-files dir))
+        (ol-discover-projects-in-dir (file-name-concat dir subdir) (- depth 1))))))
+
+
+
+
 
 (provide 'ol-project)
