29a678749080e4de7ac9c1a38067df0c492c33de
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-16 16:17:48 +0100

    Cached project name and root

--- .emacs_config/config/ol-project.el
+++ .emacs_config/config/ol-project.el
@@ -5,10 +5,24 @@
   (when-let ((root (locate-dominating-file default-directory ".git")))
     (file-truename root)))
 
+(defvar-local ol-project-root 'unknown)
+
 (defun ol-project-root ()
+  (if (eq 'unknown ol-project-root)
+      (setq ol-project-root (ol-project-root-compute))
+    ol-project-root))
+
+(defun ol-project-root-compute ()
   (run-hook-with-args-until-success 'ol-project-root-functions))
 
+(defvar-local ol-project-name 'unknown)
+
 (defun ol-project-name ()
+  (if (eq 'unknown ol-project-name)
+      (setq ol-project-name (ol-project-name-compute))
+    ol-project-name))
+
+(defun ol-project-name-compute ()
   (when-let ((root (ol-project-root)))
     (file-name-nondirectory (directory-file-name root))))
     
