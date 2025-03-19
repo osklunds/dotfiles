fc15c73c5b90fa0aaee6e687670e52e4a91aee3f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-19 20:26:32 +0100

    Clean up evil go to def

--- .emacs_config/config/ol-evil.el
+++ .emacs_config/config/ol-evil.el
@@ -221,13 +221,7 @@ and terminal variations."
 ;;;; go to definition
 ;;;;----------------------------------------------------------------------------
 
-(defun ol-evil-definition-not-found (string _position)
-  ;; (message "No definition found for: %s " string))
-  )
-
-;; Original: (evil-goto-definition-imenu evil-goto-definition-semantic evil-goto-definition-xref evil-goto-definition-search)
-(setc evil-goto-definition-functions '(evil-goto-definition-xref
-                                       ol-evil-definition-not-found))
+(setc evil-goto-definition-functions '(evil-goto-definition-xref))
 
 ;;;; ---------------------------------------------------------------------------
 ;;;; Subword
