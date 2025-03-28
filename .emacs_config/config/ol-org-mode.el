9fee6f0eefb8809b766718bd00c0a54b15c01137
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-03-28 19:27:36 +0100

    Initial work on variable pitch in org-mode

--- .emacs_config/config/ol-org-mode.el
+++ .emacs_config/config/ol-org-mode.el
@@ -54,11 +54,47 @@
 ;; -----------------------------------------------------------------------------
 ;; Looks
 ;; -----------------------------------------------------------------------------
+;; Many things inspired by https://sophiebos.io/posts/prettifying-emacs-org-mode/
 
 (setc org-ellipsis " ▾")
 
 (ol-set-face 'org-block :background
              (color-darken-name
               (face-attribute 'default :background) 3))
+
+(add-hook 'org-mode-hook 'variable-pitch-mode)
+
+(set-face-attribute 'fixed-pitch nil :family "Source Code Pro" :height 90)
+(set-face-attribute 'line-number nil :family "Source Code Pro" :height 90)
+(set-face-attribute 'line-number-current-line nil :family "Source Code Pro" :height 90)
+(set-face-attribute 'variable-pitch nil :family "Open Sans" :height 1.1 :weight 'normal)
+
+(dolist (face '((org-level-1 . 1.4)
+                (org-level-2 . 1.3)
+                (org-level-3 . 1.2)
+                (org-level-4 . 1.1)
+                (org-level-5 . 1.1)
+                (org-level-6 . 1.1)
+                (org-level-7 . 1.1)
+                (org-level-8 . 1.1)))
+  (set-face-attribute (car face) nil
+                      :font "Source Sans Pro"
+                      :foreground ol-black
+                      :weight 'bold
+                      :height (cdr face)))
+
+(require 'org-indent)
+(set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+
+(set-face-attribute 'org-block nil            :foreground nil :inherit 'fixed-pitch :height 1.0)
+(set-face-attribute 'org-code nil             :inherit '(shadow fixed-pitch) :height 1.0)
+(set-face-attribute 'org-indent nil           :inherit '(org-hide fixed-pitch) :height 1.0)
+(set-face-attribute 'org-verbatim nil         :inherit '(shadow fixed-pitch) :height 1.0)
+(set-face-attribute 'org-special-keyword nil  :inherit '(font-lock-comment-face
+                                                         fixed-pitch))
+(set-face-attribute 'org-meta-line nil        :inherit '(font-lock-comment-face fixed-pitch))
+(set-face-attribute 'org-checkbox nil         :inherit 'fixed-pitch)
+
+(setc org-hide-emphasis-markers t)
 
 (provide 'ol-org-mode)
