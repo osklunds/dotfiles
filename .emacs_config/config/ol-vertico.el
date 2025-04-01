8bf4dfa968b4a15808319bb655ad0788efc101e2
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-04-01 19:51:48 +0200

    Basic find file working

--- .emacs_config/config/ol-vertico.el
+++ .emacs_config/config/ol-vertico.el
@@ -11,8 +11,12 @@
 (vertico-mode)
 
 ;; -----------------------------------------------------------------------------
-;; Keymaps
+;; Vertico
 ;; -----------------------------------------------------------------------------
+
+;;;; ---------------------------------------------------------------------------
+;;;; Key bindings
+;;;; ---------------------------------------------------------------------------
 
 (ol-define-key ol-override-map "C-j" 'switch-to-buffer)
 
@@ -25,85 +29,43 @@
 (ol-define-key minibuffer-local-map "M-o" 'embark-collect)
 
 
-;; -----------------------------------------------------------------------------
-;; Behavior
-;; -----------------------------------------------------------------------------
+;;;; ---------------------------------------------------------------------------
+;;;; Behavior
+;;;; ---------------------------------------------------------------------------
 
 (setc vertico-count 20)
 (setc vertico-cycle t)
 
 ;; -----------------------------------------------------------------------------
+;; Consult
+;; -----------------------------------------------------------------------------
+
+(setc consult-async-min-input 0)
+(setc consult-find-args "find . -not ( -path *.git/* -prune )")
+
+(ol-define-key ol-override-map "M-q" #'consult-find)
+(ol-define-key ol-override-map "M-e" #'consult-ripgrep)
+
+;; -----------------------------------------------------------------------------
 ;; Find file name
 ;; -----------------------------------------------------------------------------
 
-(defun ol-find-file-name ()
-  (interactive)
-  (let* ((candidates (split-string (shell-command-to-string "rg --files") "\n" t))
-         (selected (completing-read
-                    "Find file name: "
-                    candidates
-                    nil
-                    t
-                    nil
-                    'ol-find-file-name
-                    )))
-    (find-file selected)))
-(ol-define-key ol-override-map "M-q" 'ol-find-file-name)
-
-(defvar ol-find-file-content-last-probe nil)
-(defvar ol-find-file-content-last-result nil)
-
-(defun ol-find-file-content ()
-  (interactive)
-  (setq ol-find-file-content-last-probe nil)
-  (setq ol-find-file-content-last-result nil)
-
-  (let* (
-         (selected (completing-read
-                    "Find file name: "
-                    'ol-find-file-content-collection
-                    nil
-                    t
-                    nil
-                    'ol-find-file-name
-                    )))
-    (find-file selected)))
-
-(defun ol-find-file-content-collection (probe pred action)
-  (let* (
-         (inhibit-message t)
-         (candidates (while-no-input
-                       (redisplay)
-                       (list 'result (process-lines-ignore-status "rg"
-                                                                  "--no-heading"
-                                                                  "--line-number"
-                                                                  "--with-filename"
-                                                                  probe))))
-         (candidates-return
-          (pcase candidates
-            (`(result ,new-candidates)
-             (progn
-               (setq ol-find-file-content-last-probe probe)
-               (setq ol-find-file-content-last-result new-candidates)
-               new-candidates))
-            ('t
-             (cond
-              ((string-equal probe ol-find-file-content-last-probe)
-               ol-find-file-content-last-result)
-              (t nil)))
-            ('nil
-             (error "todo"))))
-         )
-    
-    (cond
-     ((eq (car-safe action) 'boundaries) nil)
-     ((eq action 'metadata) nil)
-     ((eq action t) candidates-return))))
-
-(ol-define-key ol-override-map "M-e" 'ol-find-file-content)
+;; (defun ol-find-file-name ()
+;;   (interactive)
+;;   (let* ((candidates (split-string (shell-command-to-string "rg --files") "\n" t))
+;;          (selected (completing-read
+;;                     "Find file name: "
+;;                     candidates
+;;                     nil
+;;                     t
+;;                     nil
+;;                     'ol-find-file-name
+;;                     )))
+;;     (find-file selected)))
+;; (ol-define-key ol-override-map "M-q" 'ol-find-file-name)
 
 ;; -----------------------------------------------------------------------------
-;; Completion style
+;; Orderless
 ;; -----------------------------------------------------------------------------
 
 (setq completion-styles '(orderless basic)
