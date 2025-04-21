aedffd07c564d565d5b506a0f38057500d747032
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-04-21 11:01:10 +0200

    Move ol completion style into completing read own

--- .emacs_config/config_selective/ol-completing-read-own.el
+++ .emacs_config/config_selective/ol-completing-read-own.el
@@ -1,7 +1,8 @@
 ;; -*- lexical-binding: t -*-
 
 (require 'ol-evil)
-(require 'ol-completion-style)
+(require 'ol-ert)
+(require 'ol-colors)
 
 (require 'embark)
 
@@ -44,6 +45,94 @@
 ;; -----------------------------------------------------------------------------
 ;; Matcher
 ;; -----------------------------------------------------------------------------
+
+(defun ol-all-completions (string table pred _point)
+  (let* ((regex (ol-string-to-regex string))
+         (completion-regexp-list (list regex))
+         (completion-ignore-case (ol-ignore-case-p string)))
+    (setq completion-lazy-hilit-fn
+          (apply-partially #'ol-highlight-completion regex completion-ignore-case))
+    (all-completions "" table pred)))
+
+(defun ol-ignore-case-p (string)
+  (string= string (downcase string)))
+
+(defun ol-string-to-regex (string)
+  (let* ((parts (split-string string " ")))
+    (apply #'concat (cdr (mapcan (lambda (part) (list ".*?" part)) parts)))))
+
+(ert-deftest ol-string-to-regex-test ()
+  (ol-assert-equal "" (ol-string-to-regex ""))
+  (ol-assert-equal "defun" (ol-string-to-regex "defun"))
+  (ol-assert-equal "defun.*?my-fun" (ol-string-to-regex "defun my-fun"))
+  )
+
+(ert-deftest ol-all-completions-test ()
+  (let ((candidates '("read-from-string"
+                      "read-from-buffer"
+                      "read-from-minibuffer"
+                      "read"
+                      "READ"
+                      )))
+
+    (ol-assert-equal '(
+                       "read-from-string"
+                       "read-from-buffer"
+                       "read-from-minibuffer"
+                       "read"
+                       "READ"
+                       )
+                     (ol-all-completions "read" candidates nil nil))
+
+    (ol-assert-equal '(
+                       "read-from-buffer"
+                       "read-from-minibuffer"
+                       )
+                     (ol-all-completions "read buffer" candidates nil nil))
+
+    (ol-assert-equal '(
+                       "read"
+                       "READ"
+                       )
+                     (ol-all-completions "ead$" candidates nil nil))
+
+    (ol-assert-equal '(
+                       "read-from-minibuffer"
+                       )
+                     (ol-all-completions "read -[min]+" candidates nil nil))
+
+    (ol-assert-equal nil (ol-all-completions "dummy" candidates nil nil))
+
+    (ol-assert-equal '(
+                       "READ"
+                       )
+                     (ol-all-completions "D" candidates nil nil))
+    )
+  )
+
+(defun ol-try-completion (string table pred point)
+  (let ((all (ol-all-completions string table pred point)))
+    (cond
+     ((null all) nil)
+     ((eq (length all) 1) string)
+     (t string))))
+
+(add-to-list 'completion-styles-alist
+             '(ol ol-try-completion ol-all-completions "ol"))
+
+(defun ol-highlight-completion (regex ignore-case candidate)
+  (if ol-async-process
+      candidate
+    (let* ((case-fold-search ignore-case))
+      (string-match regex candidate)
+      (let* ((m (match-data))
+             (start (car m))
+             (end (cadr m)))
+        (add-face-text-property start end 'ol-match-face nil candidate))
+      candidate
+      )))
+
+(setq completion-lazy-hilit t)
 
 (setc completion-styles '(ol))
 
