b342f52e737caac18dead65e4a6e0247ee61702f
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-04-26 09:58:44 +0200

    Basic corfu config

---
+++ .emacs_config/config/ol-corfu.el
@@ -0,0 +1,39 @@
+;; -*- lexical-binding: t -*-
+
+(require 'ol-evil)
+
+(require 'corfu)
+(require 'cape)
+
+;; corfu
+
+(global-corfu-mode)
+
+(setc corfu-auto t)
+(setc corfu-auto-prefix 0)
+(setc corfu-auto-delay 0)
+
+;; Keybinds
+
+(ol-define-key corfu-map 'return #'corfu-quit)
+(ol-define-key corfu-map "C-j" #'corfu-next)
+(ol-define-key corfu-map "C-k" #'corfu-previous)
+
+(defun ol-on-completion-in-region-mode ()
+  (if completion-in-region-mode
+      (ol-override-mode -1)
+    (ol-override-mode t)))
+
+(add-hook 'completion-in-region-mode-hook #'ol-on-completion-in-region-mode)
+
+(add-hook 'evil-insert-state-exit-hook #'corfu-quit)
+
+(defun ol-complete-or-insert-tab ()
+  (interactive)
+  (if completion-in-region-mode
+      (corfu-complete)
+    (ol-insert-tab)))
+
+(ol-define-key evil-insert-state-map 'tab #'ol-complete-or-insert-tab)
+
+(provide 'ol-corfu)
