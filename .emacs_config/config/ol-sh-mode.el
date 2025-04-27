cb9aa5d34d2d28710b95703271578aa5d1a254b7
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-04-27 08:52:15 +0200

    Fix sh-mode capf

---
+++ .emacs_config/config/ol-sh-mode.el
@@ -0,0 +1,16 @@
+
+(require 'ol-corfu)
+
+(defun ol-add-sh-mode-capf ()
+  (setq-local completion-at-point-functions
+              (list (cape-capf-super #'ol-capf-abbrev
+                                     #'ol-capf-sh-mode
+                                     #'ol-capf-dabbrev))))
+
+(defun ol-capf-sh-mode ()
+  (cape-wrap-properties #'sh-completion-at-point-function
+                        :annotation-function (lambda (_) "Shell")))
+
+(add-hook 'sh-mode-hook #'ol-add-sh-mode-capf)
+
+(provide 'ol-sh-mode)
