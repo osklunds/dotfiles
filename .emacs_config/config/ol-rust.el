5aca1d2fb88309936092d4aeeca791bb73ab7b07
Oskar Lundström <21679735+osklunds@users.noreply.github.com>
2025-06-08 15:48:52 +0200

    Save silently when running rust test

--- .emacs_config/config/ol-rust.el
+++ .emacs_config/config/ol-rust.el
@@ -3,6 +3,7 @@
 (require 'ol-util)
 (require 'ol-vterm)
 (require 'ol-evil)
+(require 'ol-file)
 ;; (require 'ol-eglot)
 
 (require 'rust-mode)
@@ -58,12 +59,12 @@
 
 (defun ol-rust-cargo-check ()
   (interactive)
-  (save-buffer)
+  (ol-save-silently)
   (ol-send-cmd-to-visible-vterm-buffers "cargo check --tests"))
 
 (defun ol-rust-cargo-build ()
   (interactive)
-  (save-buffer)
+  (ol-save-silently)
   (ol-send-cmd-to-visible-vterm-buffers "cargo build --tests"))
 
 (ol-evil-define-key 'normal rust-mode-map "C-c C-c" 'ol-rust-cargo-check)
@@ -95,7 +96,7 @@
     (ol-regexp-group "^ *fn \\([a-zA-Z0-9_]+\\)" (thing-at-point 'line t) 1)))
 
 (defun ol-rust-run-test (test-name)
-  (save-buffer)
+  (ol-save-silently)
   (let ((cmd (concat "ct " test-name)))
     (setq ol-rust-last-test-name test-name)
     (ol-send-cmd-to-visible-vterm-buffers cmd)))
